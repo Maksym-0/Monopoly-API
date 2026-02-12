78979a8549c411ddef0d9d253e18af53950a4a97
Maksym <maxim27800@gmail.com>
2026-02-12 21:24:58 +0200

    Refactor board creation into BoardFactory class

---
+++ Monopoly.Core/Factories/BoardFactory.cs
@@ -0,0 +1,37 @@
+﻿using Monopoly.Core.Models.Abstractions.Classes;
+using Monopoly.Core.Models.Game;
+using Monopoly.Core.Models.Game.Cells;
+
+namespace Monopoly.Core.Factories
+{
+    internal static class BoardFactory
+    {
+        internal static GameBoard CreateBoard(Guid gameId)
+        {
+            Guid boardId = Guid.NewGuid();
+
+            List<Cell> cells = CellFactory.CreateCells(boardId);
+            List<Models.Game.Monopoly> monopolies = MonopolyFactory.CreateMonopolies(boardId);
+
+            for (int i = 0; i < cells.Count; i++)
+            {
+                if (cells[i] is CompanyCell companyCell)
+                {
+                    int monopolyIndex = Constants.CellsMonopolyIndex[i];
+                    if (monopolyIndex != 0)
+                    {
+                        Models.Game.Monopoly? monopoly = monopolies.FirstOrDefault(m => m.Index == monopolyIndex);
+                        if (monopoly == null)
+                        {
+                            throw new Exception($"Помилка при ініціалізації гри: не знайдено монополію з індексом {monopolyIndex} для клітини {companyCell.Name}");
+                        }
+                        companyCell.SetMonopoly(monopoly);
+                        monopoly.Cells.Add(companyCell);
+                    }
+                }
+            }
+
+            return new GameBoard(boardId, gameId, cells, monopolies);
+        }
+    }
+}
--- Monopoly.Core/Factories/GameFactory.cs
+++ Monopoly.Core/Factories/GameFactory.cs
@@ -1,6 +1,4 @@
-﻿using Monopoly.Core.Models.Abstractions.Classes;
-using Monopoly.Core.Models.Game;
-using Monopoly.Core.Models.Game.Cells;
+﻿using Monopoly.Core.Models.Game;
 using Monopoly.Core.Models.Room;
 
 namespace Monopoly.Core.Factories
@@ -10,30 +8,8 @@ namespace Monopoly.Core.Factories
         public static Game CreateGame(Room room)
         {
             Guid gameId = Guid.NewGuid();
-            Guid boardId = Guid.NewGuid();
-
-            List<Cell> cells = CellFactory.CreateCells(boardId);
-            List<Models.Game.Monopoly> monopolies = MonopolyFactory.CreateMonopolies(boardId);
-
-            for(int i = 0; i < cells.Count; i++)
-            {
-                if (cells[i] is CompanyCell companyCell)
-                {
-                    int monopolyIndex = Constants.CellsMonopolyIndex[i];
-                    if(monopolyIndex != 0)
-                    {
-                        Models.Game.Monopoly? monopoly = monopolies.FirstOrDefault(m => m.Index == monopolyIndex);
-                        if(monopoly == null)
-                        {
-                            throw new Exception($"Помилка при ініціалізації гри: не знайдено монополію з індексом {monopolyIndex} для клітини {companyCell.Name}");
-                        }
-                        companyCell.SetMonopoly(monopoly);
-                        monopoly.Cells.Add(companyCell);
-                    }
-                }
-            }
-
-            GameBoard board = new GameBoard(boardId, gameId, cells, monopolies);
+            
+            GameBoard board = BoardFactory.CreateBoard(gameId);
             List<Player> players = PlayerFactory.CreatePlayers(room.Players, gameId);
             TurnState turnState = new TurnState(Guid.NewGuid(), gameId);
 
